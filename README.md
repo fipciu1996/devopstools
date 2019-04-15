279cbc307cfaca39c53853a9a6c401a20fa4be34
vipek <fipciu1996@gmail.com>
2019-04-16 01:13:10 +0200

    Initial commit for zabbix python integration

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# devopstools
+# Devopstools
 My Swiss Army knife for DevOps
+
+During my work as DevOps Engineer I started using python for simplifying and automatic system administration or monitoring systems.
+Maybe it will be also useful for someone else ;)
+
+## Zabbix_problem_notify
+
+There is possibility to use integrated solutions for sending SMS notifications throught Zabbix, but if for some reasons you cannot use that. This script can be great for you. After a little bit of customization you are able to run it in backgroud and it will listen for problems in Zabbix and send you SMS with AWS service SNS.
+What you only need is boto3 and zabbix_api packages.
+
+`pip install boto3`
+
+`pip install zabbix_api`
---
+++ Zabbix_problems_notify/pyzabbix_with_aws.py
@@ -0,0 +1,89 @@
+from zabbix_api import ZabbixAPI
+import time
+import datetime
+import boto3
+from botocore.config import Config
+
+# Integration with AWS
+client = boto3.client(
+    "sns",
+    aws_access_key_id="Programmatic access key",
+    aws_secret_access_key="Programmatic secret key",
+    region_name="Region name for your sns",
+    config=Config(proxies={'proxy address'}) # you can delete it if you won't use proxy
+)
+
+numbers = ["+48123123123"] # you can pass here every person who should get notifications
+topic = client.create_topic(Name="Example") # if it doesn't created the topic will be created
+topic_arn = topic['TopicArn'] # it simplifying getting arn 
+
+# Caching function
+def memoize(function):
+    memo = {}
+
+    def wrapper(*args):
+        global date
+        cleared = False
+        if not cleared:
+            if date.minute == 3:
+                memoize.memo = {}
+                cleared = True
+
+        if args in memo:
+            return memo[args]
+        else:
+            rv = function(*args)
+            memo[args] = rv
+            return rv
+    return wrapper
+
+
+# Simple Contact Policy for notifications
+def scheduler():
+    date = datetime.datetime.now()
+    if date.hour > 7 < 19:
+        return True
+    else:
+        return False
+
+
+# Function for sending messages (message:string, numbers:array, topic:topic, topic_arn:topic_arn)
+@memoize
+def send_message(message, numbers, topic, topic_arn):
+    for number in numbers:
+        client.subscribe(
+            TopicArn=topic_arn,
+            Protocol='sms',
+            Endpoint=number  #  number who'll receive an SMS message.
+        )
+    client.publish(Message=message, TopicArn=topic_arn, MessageAttributes={
+        'AWS.SNS.SMS.SenderID': {
+            'DataType': 'String',
+            'StringValue': 'Zabbix'
+        }
+    })
+
+groups = ["some groups names"] # you can specify groups for filtraiting triggers
+
+zapi = ZabbixAPI(server="https://zbxmain.example.com") # declare your zabbix address
+zapi.login("username", "password") # basic authentication for zabbix frontend
+print("Connected to Zabbix API Version %s" % zapi.api_version())
+
+
+while True:
+    if scheduler():
+	for group in groups:
+		triggers = zapi.trigger.get({
+                "only_true": 1,
+                "skipDependent": 1,
+                "group": group,
+                "monitored": 1,
+                "active": 1,
+                "output": "extend",
+                "expandDescription": 1
+                })
+        	for t in triggers:
+            		if int(t['value']) == 1:
+                		send_message(t['description'], numbers, topic, topic_arn)
+                		time.sleep(900)
+
